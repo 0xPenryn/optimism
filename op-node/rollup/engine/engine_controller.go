fe0ca494a230a445c448e7c50599a542260bc2ba
0xPenryn <29718876+0xPenryn@users.noreply.github.com>
2026-04-28 15:54:26 -0400

    Revert "update"
    This reverts commit 4c8a13992bb83b6e63b79912943b946e0487a9e0.

--- op-node/rollup/engine/engine_controller.go
+++ op-node/rollup/engine/engine_controller.go
@@ -628,7 +628,8 @@ func (e *EngineController) insertUnsafePayload(ctx context.Context, envelope *et
 		fc.FinalizedBlockHash = e.FinalizedHead().Hash
 	}
 
-	e.log.Info("sync status", "status", e.syncStatus)
+	e.log.Info("Forkchoice state", fc)
+	e.log.Info("sync status", e.syncStatus)
 
 	if e.syncStatus == syncStatusFinishedELButNotFinalized {
 		offsetRef := ref
@@ -662,9 +663,6 @@ func (e *EngineController) insertUnsafePayload(ctx context.Context, envelope *et
 	logFn := e.logSyncProgressMaybe()
 	defer logFn()
 	fcu2Start := time.Now()
-
-	e.log.Info("Forkchoice state", "fc", fc)
-
 	fcRes, err := e.engine.ForkchoiceUpdate(ctx, &fc, nil)
 	if err != nil {
 		var rpcErr rpc.Error
