c1a65da1dd21f2a62980d5209a7831fd5889aa73
Sebastian Stammler <seb@oplabs.co>
2026-04-28 15:15:45 +0200

    op-node: only set FCU finalized after initial EL-sync

--- op-node/rollup/engine/engine_controller.go
+++ op-node/rollup/engine/engine_controller.go
@@ -620,9 +620,12 @@ func (e *EngineController) insertUnsafePayload(ctx context.Context, envelope *et
 
 	// Mark the new payload as valid
 	fc := eth.ForkchoiceState{
-		HeadBlockHash:      envelope.ExecutionPayload.BlockHash,
-		SafeBlockHash:      e.SafeL2Head().Hash,
-		FinalizedBlockHash: e.FinalizedHead().Hash,
+		HeadBlockHash: envelope.ExecutionPayload.BlockHash,
+		SafeBlockHash: e.SafeL2Head().Hash,
+	}
+	// only set finalized after initial EL sync
+	if !e.isEngineInitialELSyncing() {
+		fc.FinalizedBlockHash = e.FinalizedHead().Hash
 	}
 	if e.syncStatus == syncStatusFinishedELButNotFinalized {
 		offsetRef := ref
